43d54baba5d899b697315f905dbb2b1e4f2e543e
Aarav <anshuk3917@gmail.com>
2023-03-17 14:05:47 +0530

    Added graph

--- src/components/Charts/Pie.jsx
+++ src/components/Charts/Pie.jsx
@@ -1,46 +1,14 @@
 import React from 'react';
-import { AccumulationChartComponent, AccumulationSeriesCollectionDirective, AccumulationSeriesDirective, AccumulationLegend, PieSeries, AccumulationDataLabel, Inject, AccumulationTooltip } from '@syncfusion/ej2-react-charts';
 
-import { useStateContext } from '../../contexts/ContextProvider';
+const PieChart = () => (
+  <iframe
+    src="https://dune.com/embeds/1535829/2916052"
+    title="Proposal Chart"
+    width="100%"
+    height="500px"
+    frameBorder="0"
+    allowfullscreen
+  />
+);
 
-const Doughnut = ({ id, data, legendVisiblity, height }) => {
-  const { currentMode } = useStateContext();
-
-  return (
-    <AccumulationChartComponent
-      id={id}
-      legendSettings={{ visible: legendVisiblity, background: 'white' }}
-      height={height}
-      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
-      tooltip={{ enable: true }}
-    >
-      <Inject services={[AccumulationLegend, PieSeries, AccumulationDataLabel, AccumulationTooltip]} />
-      <AccumulationSeriesCollectionDirective>
-        <AccumulationSeriesDirective
-          name="Sale"
-          dataSource={data}
-          xName="x"
-          yName="y"
-          innerRadius="40%"
-          startAngle={0}
-          endAngle={360}
-          radius="70%"
-          explode
-          explodeOffset="10%"
-          explodeIndex={2}
-          dataLabel={{
-            visible: true,
-            name: 'text',
-            position: 'Inside',
-            font: {
-              fontWeight: '600',
-              color: '#fff',
-            },
-          }}
-        />
-      </AccumulationSeriesCollectionDirective>
-    </AccumulationChartComponent>
-  );
-};
-
-export default Doughnut;
+export default PieChart;
