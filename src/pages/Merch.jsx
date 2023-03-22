e5f1ce3876509bed5162022883e285e4f26b2cfd
kamalishere <kamaleinstein1@gmail.com>
2023-03-22 05:12:28 +0000

    merch upgrade

--- src/pages/Merch.jsx
+++ src/pages/Merch.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
@@ -28,73 +29,55 @@ const ExpandMore = styled((props) => {
 }));
 
 export default function RecipeReviewCard() {
-  const [expanded, setExpanded] = React.useState(false);
+  const [expanded, setExpanded] = React.useState([]);
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
+  const handleExpandClick = (index) => {
+    setExpanded((prevExpanded) => {
+      const newExpanded = [...prevExpanded];
+      newExpanded[index] = !newExpanded[index];
+      return newExpanded;
+    });
   };
 
   return (
-    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+    <>
+    <h1 className='text-sky-400 text-center text-6xl'>APE Merch</h1>
+    <div style={{ display: 'flex', flexWrap: 'wrap' }} className="my-6">
       {data.map((item, index) => (
         <Card key={index} sx={{ maxWidth: 345 }}>
-          <CardHeader
-          avatar={
-            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-              A
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title={item.title}
-          subheader="Sell"
-        />
-        <CardMedia
-          component="img"
-          height="583"
-          width="583"
-          image={item.image}
-          alt="Paella dish"
-        />
-        <CardContent>
-          <Typography variant="body2" color="text.secondary">
-            {item.description}
-          </Typography>
-        </CardContent>
-        <Button>{`Buy for ${item.price}`}</Button>
-        <CardActions disableSpacing>
-          <ExpandMore
-            expand={expanded}
-            onClick={handleExpandClick}
-            aria-expanded={expanded}
-            aria-label="show more"
-          >
-          <ExpandMoreIcon />
-          </ExpandMore>
-        </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardMedia
+            component="img"
+            height="583"
+            width="583"
+            image={item.image}
+            alt="Paella dish"
+          />
           <CardContent>
-            <Typography paragraph>Method:</Typography>
-            <Typography paragraph>
-              Heat 1/2
-            </Typography>
-            <Typography paragraph>
-              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet ove.
-            </Typography>
-            <Typography paragraph>
-              Add rice and stir very gently to distribute. Top with artichokes and
-              
-            </Typography>
-            <Typography>
-              Set aside off of the heat to let rest for 10 minutes, and then serve.
+            <Typography variant="body2" color="text.secondary">
+              {item.subheader}
             </Typography>
           </CardContent>
-        </Collapse>
-    </Card>
-     ))}
+          <Button>{`Buy for ${item.price} APECOIN`}</Button>
+          <CardActions disableSpacing>
+            <ExpandMore
+              expand={expanded[index]}
+              onClick={() => handleExpandClick(index)}
+              aria-expanded={expanded[index]}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </ExpandMore>
+          </CardActions>
+          <Collapse in={expanded[index]} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                {item.description}
+              </Typography>
+            </CardContent>
+          </Collapse>
+        </Card>
+      ))}
     </div>
+    </>
   );
 }
