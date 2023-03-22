e461d88ec14607ef1294d76672bdb6f3ec9514bc
kamalishere <kamaleinstein1@gmail.com>
2023-03-22 15:28:56 +0000

    bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import React, { useEffect , useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
@@ -56,80 +55,80 @@ const App = () => {
     <div className={currentMode === 'Dark' ? 'dark' : ''}>
       <EnvContext.Provider value={{ env, isCAIP }}>
         {checkForWeb3Data(web3Data) ? (
-        <Web3Context.Provider value={web3Data}>
-          <BrowserRouter>
-            <div className="flex relative dark:bg-main-dark-bg">
-              <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
-                <TooltipComponent
-                  content="Settings"
-                >
-                  <button
-                    type="button"
-                    onClick={() => setThemeSettings(true)}
-                    style={{ background: currentColor, borderRadius: '50%' }}
-                    className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+          <Web3Context.Provider value={web3Data}>
+            <BrowserRouter>
+              <div className="flex relative dark:bg-main-dark-bg">
+                <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+                  <TooltipComponent
+                    content="Settings"
                   >
-                    <FiSettings />
-                  </button>
+                    <button
+                      type="button"
+                      onClick={() => setThemeSettings(true)}
+                      style={{ background: currentColor, borderRadius: '50%' }}
+                      className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+                    >
+                      <FiSettings />
+                    </button>
 
-                </TooltipComponent>
+                  </TooltipComponent>
+                </div>
+                {activeMenu ? (
+                  <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+                    <Sidebar />
+                  </div>
+                ) : (
+                  <div className="w-0 dark:bg-secondary-dark-bg">
+                    <Sidebar />
+                  </div>
+                )}
+                <div
+                  className={
+                    activeMenu
+                      ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+                      : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+                  }
+                >
+                  <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+                    <Navbar />
+                  </div>
+                  <div>
+                    {themeSettings && (<ThemeSettings />)}
+
+                    <Routes>
+                      {/* dashboard  */}
+                      <Route path="/" element={(<Ecommerce />)} />
+                      <Route path="/analyse" element={(<Ecommerce />)} />
+
+                      {/* pages  */}
+                      <Route path="/merch" element={<Merch />} />
+                      <Route path="/merch" element={<Merch />} />
+                      <Route path="/proposals" element={<Home />} />
+
+                      {/* apps  */}
+                      <Route path="/kanban" element={<Kanban />} />
+                      <Route path="/calendar" element={<Calendar />} />
+                      <Route path="/home" element={<Home />} />
+
+                      {/* charts  */}
+                      <Route path="/line" element={<Line />} />
+                      <Route path="/area" element={<Area />} />
+                      <Route path="/bar" element={<Bar />} />
+                      <Route path="/pie" element={<Pie />} />
+                      <Route path="/financial" element={<Financial />} />
+                      <Route path="/color-mapping" element={<ColorMapping />} />
+                      <Route path="/apeholders" element={<ApeHolder />} />
+                      <Route path="/trustlevel" element={<TrustedUsers />} />
+                      <Route path="/chat" element={<ChatTest />} />
+                      <Route path="/chats" element={<GetChatsTest />} />
+                      <Route path="/pushchat" element={<PushChat />} />
+                    </Routes>
+                  </div>
+                  <Footer />
+                </div>
               </div>
-              {activeMenu ? (
-                <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
-                  <Sidebar />
-                </div>
-              ) : (
-                <div className="w-0 dark:bg-secondary-dark-bg">
-                  <Sidebar />
-                </div>
-              )}
-              <div
-                className={
-                  activeMenu
-                    ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
-                    : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
-                }
-              >
-                <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
-                  <Navbar />
-                </div>
-                <div>
-                  {themeSettings && (<ThemeSettings />)}
-
-                  <Routes>
-                    {/* dashboard  */}
-                    <Route path="/" element={(<Ecommerce />)} />
-                    <Route path="/analyse" element={(<Ecommerce />)} />
-
-                    {/* pages  */}
-                    <Route path="/merch" element={<Merch />} />
-                    <Route path="/merch" element={<Merch />} />
-                    <Route path="/proposals" element={<Home />} />
-
-                    {/* apps  */}
-                    <Route path="/kanban" element={<Kanban />} />
-                    <Route path="/calendar" element={<Calendar />} />
-                    <Route path="/home" element={<Home />} />
-
-                    {/* charts  */}
-                    <Route path="/line" element={<Line />} />
-                    <Route path="/area" element={<Area />} />
-                    <Route path="/bar" element={<Bar />} />
-                    <Route path="/pie" element={<Pie />} />
-                    <Route path="/financial" element={<Financial />} />
-                    <Route path="/color-mapping" element={<ColorMapping />} />
-                    <Route path="/apeholders" element={<ApeHolder />} />
-                    <Route path="/trustlevel" element={<TrustedUsers />} />
-                    <Route path="/chat" element={<ChatTest />} />
-                    <Route path="/chats" element={<GetChatsTest />} />
-                    <Route path="/pushchat" element={<PushChat />} />
-                  </Routes>
-                </div>
-                <Footer />
-              </div>
-            </div>
-          </BrowserRouter>
-      </Web3Context.Provider>
+            </BrowserRouter>
+          </Web3Context.Provider>
         ) : null}
       </EnvContext.Provider>
     </div>
