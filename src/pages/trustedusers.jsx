f3c884f7f277182d2858a93e775a2bdca33b9b53
Aarav <anshuk3917@gmail.com>
2023-03-22 09:15:36 +0530

    Added Trust_level

--- src/pages/trustedusers.jsx
+++ src/pages/trustedusers.jsx
@@ -1,34 +1,52 @@
-/* eslint-disable */
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material"
+import data from "./trust.json";
+console.log(data)
 
-const TrustedUsers = () => {
-  const [trustedUsers, setTrustedUsers] = useState([]);
-
-  useEffect(() => {
-    fetch('https://forum.apecoin.com/directory_items.json?period=all')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        const result = data.directory_items.map(item => ({
-          username: item.user.username,
-          trust_level: item.user.trust_level
-        }));
-        setTrustedUsers(result);
-      })
-      .catch(error => console.error(error));
-  }, []);
-
+const UserDataTable = ({ data }) => {
   return (
-    <div>
-      <h1>Trusted Users</h1>
-      {trustedUsers.map(user => (
-        <div key={user.username}>
-          <h2>{user.username}</h2>
-          <p>Trust level: {user.trust_level}</p>
-        </div>
-      ))}
-    </div>
-  )
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>User ID</TableCell>
+            <TableCell>Likes Received</TableCell>
+            <TableCell>Likes Given</TableCell>
+            <TableCell>Topic Count</TableCell>
+            <TableCell>Post Count</TableCell>
+            <TableCell>Topics Entered</TableCell>
+            <TableCell>Posts Read</TableCell>
+            <TableCell>Days Visited</TableCell>
+            <TableCell>Solutions</TableCell>
+            <TableCell>Time Read</TableCell>
+            <TableCell>User Info</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        {data.directory_items.map((user) => (
+            <TableRow >
+              <TableCell>{user.id}</TableCell>
+              <TableCell>{user.likes_received}</TableCell>
+              <TableCell>{user.likes_given}</TableCell>
+              <TableCell>{user.topic_count}</TableCell>
+              <TableCell>{user.post_count}</TableCell>
+              <TableCell>{user.topics_entered}</TableCell>
+              <TableCell>{user.posts_read}</TableCell>
+              <TableCell>{user.days_visited}</TableCell>
+              <TableCell>{user.solutions}</TableCell>
+              <TableCell>{user.time_read}</TableCell>
+              <TableCell>
+                <img src={user.user.avatar_template.replace('{size}', '40')} alt="avatar" />
+                <span>{user.user.username}</span>
+                <span>{user.user.name}</span>
+                <span>{user.user.title}</span>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 };
 
-export default TrustedUsers;
+export default UserDataTable;
