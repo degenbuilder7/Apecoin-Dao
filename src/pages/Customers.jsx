4e8b4ce46f74bb4c11dbf0100ab4498988423db3
Aarav <anshuk3917@gmail.com>
2023-03-23 08:29:26 +0530

    Impove amalyse

---
+++ src/pages/Customers.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Customers = () => {
+  return (
+    <iframe
+    src="https://dune.com/embeds/989133/1712934"
+    title="Proposal Chart"
+    width="100%"
+    height="500px"
+    frameBorder="0"
+    allowfullscreen
+  />
+  );
+};
+
+export default Customers;
