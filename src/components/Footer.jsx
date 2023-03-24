dd09d87cbf578e0bb5c3dbe8206f12ae720b03e7
Aarav <anshuk3917@gmail.com>
2023-03-24 10:49:38 +0530

    improved Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
 const Footer = () => (
-  <div className="mt-24">
-    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+  <div className="mt-24 flex flex-col items-center">
+    <p className="dark:text-gray-200 text-gray-700 text-center mb-10">
       © 2023 All rights reserved by ApeCoinDao.eth
     </p>
+    <div className="flex flex-col items-center">
+      <p className="dark:text-gray-200 text-gray-700 text-center mb-2">
+        Join our community
+      </p>
+      <a href="https://discord.gg/wg0" >
+        <img src="https://cdn.discordapp.com/attachments/1033079854826000484/1088692939607261244/discord_logo-removebg-preview.png" alt="Join us on Discord" width={50} height={50} />
+      </a>
+    </div>
   </div>
 );
 
