2521dbe0dea7a1bb28148700876191ab6bbcdb76
kamalishere <kamaleinstein1@gmail.com>
2023-03-24 05:55:18 +0000

    token

---
+++ auth.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import { ThirdwebAuth } from "@thirdweb-dev/auth/next";
+import { PrivateKeyWallet } from "@thirdweb-dev/auth/evm";
+import { domainName } from "./const/yourDetails";
+
+export const { ThirdwebAuthHandler, getUser } = ThirdwebAuth({
+  domain: domainName,
+  wallet: new PrivateKeyWallet(process.env.THIRDWEB_AUTH_PRIVATE_KEY || ""),
+});
---
+++ src/utils/Checkbalance.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+import { isFeatureEnabled } from '@thirdweb-dev/sdk';
+import {
+  contractAddress,
+  erc1155TokenId,
+  minimumBalance,
+} from '../consts/yourDetails';
+
+export default async function checkBalance(sdk, address) {
+  const contract = await sdk.getContract(
+    contractAddress // replace this with your contract address
+  );
+
+  let balance;
+
+  if (isFeatureEnabled(contract.abi, 'ERC1155')) {
+    balance = await contract.erc1155.balanceOf(address, erc1155TokenId);
+  } else if (isFeatureEnabled(contract.abi, 'ERC721')) {
+    balance = await contract.erc721.balanceOf(address);
+  } else if (isFeatureEnabled(contract.abi, 'ERC20')) {
+    balance = (await contract.erc20.balanceOf(address)).value;
+    return balance.gte((minimumBalance * 1e18).toString());
+  }
+
+  // gte = greater than or equal to
+  return balance.gte(minimumBalance);
+}
