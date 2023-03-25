f1edef49248a4c0997469d8d702d82f7887aa955
Aarav <anshuk3917@gmail.com>
2023-03-25 09:41:16 +0530

    make looks good

--- src/components/Chat/History.tsx
+++ src/components/Chat/History.tsx
@@ -11,7 +11,7 @@ import Web3Context from '../../contexts/web3context';
 import EnvContext from '../../contexts/envContext';
 import * as PushAPI from '@pushprotocol/restapi';
 // import { walletToPCAIP10 } from '../..data/helpers';
-import ChatTest from './ChatTest';
+import Chat from './Chat';
 import React from 'react';
 
 const History = () => {
@@ -21,6 +21,11 @@ const History = () => {
   const [response, setResponse] = useState<any>({});
   const [threadhash, setThreadhash] = useState<string>('bafyreihzbtwys4z6ayebh34eu4qvkz5rhhnry4rwoito623bkur6lsmxvy');
   const [limit, setLimit] = useState<number>(10);
+  const [selectedMessage, setSelectedMessage] = useState(null);
+
+  const handleSelectMessage = (message) => {
+    setSelectedMessage(message);
+  };
 
 
   const updateFetchLimit = (e: React.SyntheticEvent<HTMLElement>) => {
@@ -60,7 +65,7 @@ const History = () => {
 
   return (
     <div>
-      <ChatTest />
+      <Chat />
       <h2>History Test page</h2>
 
       <Loader show={isLoading} />
@@ -75,17 +80,39 @@ const History = () => {
         <SectionItem>
         <div>
             {response ? (
-                <CodeFormatter>
-                {Object.entries(response).map(([key, value]) => (
-                    <div key={key}>
-                    <strong>From {JSON.stringify(value.fromDID, null, 4)}</strong>
-                    <br />
-                    {JSON.stringify(value.messageContent, null, 4)}
-                    <br />
-                    <p>AT: {new Date(value.timestamp).toLocaleString()}</p>
+                <div className="flex justify-between">
+                <div className="w-2/3 pr-4">
+                  <div className="h-screen overflow-y-scroll">
+                    {Object.entries(response).map(([key, value]) => (
+                      <div
+                        key={key}
+                        onClick={() => handleSelectMessage(value)}
+                        className="border rounded-lg cursor-pointer p-4 mb-4 hover:bg-gray-100 transition-colors duration-200"
+                      >
+                        <div className="flex justify-between mb-2">
+                          <strong>From {JSON.stringify(value.fromDID, null, 4)}</strong>
+                          <p className="text-right text-gray-500 text-sm">{new Date(value.timestamp).toLocaleString()}</p>
+                        </div>
+                        <div className="truncate">{JSON.stringify(value.messageContent, null, 4)}</div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+                <div className="w-1/3">
+                  {selectedMessage && (
+                    <div className="border rounded-lg p-4">
+                      <button
+                        className="absolute right-0 top-0 mr-2 mt-2 px-2 py-1 rounded-full text-white bg-gray-500 hover:bg-gray-600"
+                        onClick={() => setSelectedMessage(null)}
+                      >
+                        Close
+                      </button>
+                      <h2 className="mb-2">{JSON.stringify(selectedMessage.messageContent.title)}</h2>
+                      <div className="text-pink-600">{JSON.stringify(selectedMessage.messageContent.content, null, 4)}</div>
                     </div>
-                ))}
-                </CodeFormatter>
+                  )}
+                </div>
+              </div>
             ) : null}
         </div>
         </SectionItem>
